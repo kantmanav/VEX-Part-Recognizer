13ade9b102d61c2fd381e50c447e13bd47f58ecf
Manav Kant <Manav@Manavs-MacBook-Pro.local>
2019-04-10 21:49:04 -0700

    Add break statementand label to for-in loop

--- SeeFood-CoreML/ViewController.swift
+++ SeeFood-CoreML/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var searchLabel: UILabel!
     
     var labelText = ""
-    let parts = ["12 Tooth Gear", "24 Tooth Gear", "Motor", "36 Tooth Gear", "Axle", "Base Plate", "C-Channel", "Vex Chain", "Vex Cortex"]
+    
     
     @IBAction func seeDetails(_ sender: Any) {
         labelText = self.navigationItem.title!
@@ -56,69 +56,69 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func detect(image: CIImage) {
         
-        // Load the ML model through its generated class
-        guard let model = try? VNCoreMLModel(for: VEX().model) else {
-            fatalError("can't load ML model")
-        }
+            let parts = ["12 Tooth Gear", "24 Tooth Gear", "Motor", "36 Tooth Gear", "Axle", "Base Plate", "C-Channel", "Vex Chain", "Vex Cortex"]
         
-        let request = VNCoreMLRequest(model: model) { request, error in
-            guard let results = request.results as? [VNClassificationObservation],
-                let topResult = results.first
-                else {
-                    fatalError("unexpected result type from VNCoreMLRequest")
-                }
-            
-            for part in self.parts {
-                if topResult.identifier.contains(part) {
-                    DispatchQueue.main.async {
-                        self.navigationItem.title = part
-                        self.navigationController?.navigationBar.barTintColor = UIColor.green
-                        self.navigationController?.navigationBar.isTranslucent = false
-                        self.hide_buttons()
+            // Load the ML model through its generated class
+            guard let model = try? VNCoreMLModel(for: VEX().model) else {
+                fatalError("can't load ML model")
+            }
+        
+            let request = VNCoreMLRequest(model: model) { request, error in
+                guard let results = request.results as? [VNClassificationObservation],
+                    let topResult = results.first
+                    else {
+                        fatalError("unexpected result type from VNCoreMLRequest")
                     }
+                
+                iterateParts: for part in parts {
+                    if topResult.identifier.contains(part) {
+                        DispatchQueue.main.async {
+                            self.navigationItem.title = part
+                            self.navigationController?.navigationBar.barTintColor = UIColor.green
+                            self.navigationController?.navigationBar.isTranslucent = false
+                            self.hide_buttons()
+                        }
+                        break iterateParts
+                    }
+                }
             }
+                let handler = VNImageRequestHandler(ciImage: image)
+        
+                do { try handler.perform([request]) }
+                catch { print(error) }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        
+            if let image = info[.originalImage] as? UIImage {
+                
+                imageView.image = image
+                
+                imagePicker.dismiss(animated: true, completion: nil)
+                
+                
+                guard let ciImage = CIImage(image: image) else {
+                    fatalError("couldn't convert uiimage to CIImage")
+                }
+                
+                detect(image: ciImage)
             
+            }
         }
         
-        let handler = VNImageRequestHandler(ciImage: image)
         
-        do { try handler.perform([request]) }
-        catch { print(error) }
     }
-        
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        
-        if let image = info[.originalImage] as? UIImage {
-                    
-            imageView.image = image
-            
-            imagePicker.dismiss(animated: true, completion: nil)
-            
-            
-            guard let ciImage = CIImage(image: image) else {
-                fatalError("couldn't convert uiimage to CIImage")
-            }
-            
-            detect(image: ciImage)
-            
-        }
-    }
-        
-
-    func cameraTapped(_ sender: Any) {
-        
+    
+    @IBAction func cameraTapped(_ sender: Any) {
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
         
         present(imagePicker, animated: true, completion: nil)
     }
-   
-}
-
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
-}
+    
+    
+    // Helper function inserted by Swift 4.2 migrator.
+    fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+        return input.rawValue
+    }
 }
